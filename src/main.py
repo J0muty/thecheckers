c852a823a53fbca8a78ca4b28f00496140734213
Христофор <J0muty@mail.ru>
2025-07-27 19:24:42 +0300

    reload

--- src/main.py
+++ src/main.py
@@ -50,4 +50,4 @@ app.mount("/static", static_files, name="static")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=1337)
+    uvicorn.run("main:app", host="127.0.0.1", port=1337, reload=True)
